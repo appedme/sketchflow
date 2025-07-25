631daf00820a289eada611939c726420b28f93a6
SH20RAJ <shaswatraj3@gmail.com>
2025-07-25 07:40:01 +0530

    improve in 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,26 @@
 "use client";
+import { Button } from "@/components/ui/button";
 // import { useTheme } from "next-themes";
 import { Glitchy404 } from "@/components/ui/glitcgy-text";
+import { Backpack } from "lucide-react";
+import Link from "next/link";
 
 export default function Page404() {
 
-//   const { theme } = useTheme();
-  return (
-    <div className="min-h-screen overflow-hidden flex items-center justify-center">
-        <Glitchy404 width={800} height={232} 
-        // color={theme === "dark"? "#fff" : "#000"}
-         />
-    </div>
-  );
+    //   const { theme } = useTheme();
+    return (
+        <div className="min-h-screen overflow-hidden flex flex-col items-center justify-center gap-8">
+            <Glitchy404 width={800} height={232}
+            // color={theme === "dark"? "#fff" : "#000"}
+            />
+            <Link
+                href="/dashboard"
+                className=""
+            >
+                <Button>
+                    <Backpack /> Go to Dashboard
+                </Button>
+            </Link>
+        </div>
+    );
 }
