2137746406a4411d577cba48f4bf54a4a7d82e4a
SH20RAJ <shaswatraj3@gmail.com>
2025-08-08 09:29:37 +0530

    deleted unused files

--- src/instrumentation.js
+++
@@ -1,9 +0,0 @@
-// This file is intentionally minimal to prevent instrumentation hook errors
-// when deploying to Cloudflare Workers
-
-export function register() {
-    // This is intentionally empty
-}
-
-// Export an empty object as default
-export default {};
